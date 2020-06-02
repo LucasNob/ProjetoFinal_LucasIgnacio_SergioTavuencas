a547dd848a9d52da04ced6bdb0e89c9d4a31ef51
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2020-06-02 15:10:35 -0300

    Repository

---
+++ src/main/java/com/projetofinal/projetofinalnobregavicente/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.projetofinal.projetofinalnobregavicente.repository;
+
+import com.projetofinal.projetofinalnobregavicente.entity.Funcionario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+    
+}
---
+++ src/main/java/com/projetofinal/projetofinalnobregavicente/repository/SalaoRepository.java
@@ -0,0 +1,9 @@
+package com.projetofinal.projetofinalnobregavicente.repository;
+
+import com.projetofinal.projetofinalnobregavicente.entity.Salao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaoRepository extends JpaRepository<Salao, Integer> {
+    
+}

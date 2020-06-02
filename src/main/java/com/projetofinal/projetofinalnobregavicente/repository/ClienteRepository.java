a2f5a7ee19c46f81bc905f82e1f3730766be4569
LucasNob <lucasignacionobrega@gmail.com>
2020-06-02 14:24:00 -0300

    commit

--- src/main/java/com/projetofinal/projetofinalnobregavicente/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetofinal.projetofinalnobregavicente.repository;
-
-import com.projetofinal.projetofinalnobregavicente.entity.Cliente;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente,Integer>{
-    
-}
--- src/main/java/com/projetofinal/projetofinalnobregavicente/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetofinal.projetofinalnobregavicente.repository;
-
-import com.projetofinal.projetofinalnobregavicente.entity.Funcionario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario,Integer> {
-    
-}
